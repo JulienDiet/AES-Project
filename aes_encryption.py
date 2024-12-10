dd4094f6839a692f25fe362e49b79fc8904c26cf
JulienDiet <juliendiet7@gmail.com>
2024-12-10 17:04:04 +0100

    AES ECB DONE !!!!!

--- aes_encryption.py
+++ aes_encryption.py
@@ -1,7 +1,7 @@
-from data_structure import word128bits_to_bytes_matrix, column_to_matrix, bytes_matrix_to_word128bits
-from substitution import load_substitution_box, inverse_substitution_box
-from aes_key_schedule import key_schedule
-from roundfunction import aes_round, aes_final_round, aes_inverse_round, aes_inverse_final_round, matrix_xor
+from data_structure import *
+from aes_key_schedule import *
+from roundfunction import *
+from substitution import inverse_substitution_box, load_substitution_box
 
 
 def compute_round_count(aes_key_length):
@@ -22,74 +22,65 @@ def compute_round_count(aes_key_length):
 
 def aes_encrypt_block(block, key, aes_key_length):
     """
-    AES encryption for a single block or a list of blocks.
-    :param block: 128-bit block(s) to be encrypted (int or list of ints)
+    AES encryption for a single block.
+    :param block: 128-bit block to be encrypted
     :param key: 128/192/256 bits to encrypt the block
     :param aes_key_length: the number of bits as an integer value
-    :return: the encrypted 128-bit block(s)
+    :return: the encrypted 128-bit block
     """
-    encrypted_blocks = []
-    if isinstance(block, int):
-        block = [block]
-
     substitution_box = load_substitution_box()
     round_keys = key_schedule(key, aes_key_length, substitution_box)
     num_rounds = compute_round_count(aes_key_length)
 
-    for single_block in block:
-        block_matrix = word128bits_to_bytes_matrix(single_block)
-        round_key_matrices = [column_to_matrix(round_keys[i], round_keys[i + 1], round_keys[i + 2], round_keys[i + 3])
-                              for i in range(0, len(round_keys), 4)]
+    # Convert block to matrix
+    block_matrix = word128bits_to_bytes_matrix(block)
 
-        # Initial XOR
-        block_matrix = matrix_xor(block_matrix, round_key_matrices[0])
+    # Initial XOR
+    block_matrix = matrix_xor(block_matrix, column_to_matrix(*round_keys[:4]))
 
-        # Execute rounds
-        for i in range(1, num_rounds):
-            block_matrix = aes_round(block_matrix, round_key_matrices[i], substitution_box)
+    # Execute rounds
+    for round in range(1, num_rounds):
+        round_key_matrix = column_to_matrix(*round_keys[round * 4:(round + 1) * 4])
+        block_matrix = aes_round(block_matrix, round_key_matrix, substitution_box)
 
-        # Final round
-        block_matrix = aes_final_round(block_matrix, round_key_matrices[num_rounds], substitution_box)
+    # Final round
+    final_key_matrix = column_to_matrix(*round_keys[num_rounds * 4:(num_rounds + 1) * 4])
+    block_matrix = aes_final_round(block_matrix, final_key_matrix, substitution_box)
 
-        # Convert matrix back to 128-bit word
-        encrypted_blocks.append(bytes_matrix_to_word128bits(block_matrix))
-
-    return encrypted_blocks
+    # Convert matrix back to 128-bit word
+    encrypted_block = bytes_matrix_to_word128bits(block_matrix)
+    return encrypted_block
 
 
 def aes_decrypt_block(block, key, aes_key_length):
     """
-    AES decryption for a single block or a list of blocks.
-    :param block: the 128-bit cipher block(s) to be decrypted (int or list of ints)
+    AES decryption for a single block.
+    :param block: the 128-bit cipher block to be decrypted
     :param key: 128/192/256 bits to decrypt the cipher block
     :param aes_key_length: the number of bits as an integer value
-    :return: decrypted 128-bit block(s)
+    :return: decrypted 128-bit block
     """
-    decrypted_blocks = []
-    if isinstance(block, int):
-        block = [block]
-
     substitution_box = load_substitution_box()
-    inverse_sub_box = inverse_substitution_box(substitution_box)
+    inverse = inverse_substitution_box(substitution_box)
     round_keys = key_schedule(key, aes_key_length, substitution_box)
     num_rounds = compute_round_count(aes_key_length)
 
-    for single_block in block:
-        block_matrix = word128bits_to_bytes_matrix(single_block)
-        round_key_matrices = [column_to_matrix(round_keys[i], round_keys[i + 1], round_keys[i + 2], round_keys[i + 3])
-                              for i in range(0, len(round_keys), 4)]
+    # Convert block to matrix
+    block_matrix = word128bits_to_bytes_matrix(block)
 
-        # Initial XOR
-        block_matrix = matrix_xor(block_matrix, round_key_matrices[num_rounds])
+    # Final round (inverse final round)
+    final_key_matrix = column_to_matrix(*round_keys[num_rounds * 4:(num_rounds + 1) * 4])
+    block_matrix = aes_inverse_final_round(block_matrix, final_key_matrix, inverse)
 
-        # Execute rounds
-        for i in range(num_rounds - 1, 0, -1):
-            block_matrix = aes_inverse_round(block_matrix, round_key_matrices[i], inverse_sub_box)
+    # Execute rounds in reverse order
+    for round in range(num_rounds - 1, 0, -1):
+        round_key_matrix = column_to_matrix(*round_keys[round * 4:(round + 1) * 4])
+        block_matrix = aes_inverse_round(block_matrix, round_key_matrix, inverse)
 
-        # Final round
-        block_matrix = aes_inverse_final_round(block_matrix, round_key_matrices[0], inverse_sub_box)
+    # Initial XOR
+    initial_key_matrix = column_to_matrix(*round_keys[:4])
+    block_matrix = matrix_xor(block_matrix, initial_key_matrix)
 
-        # Convert matrix back to 128-bit word
-        decrypted_blocks.append(bytes_matrix_to_word128bits(block_matrix))
-
-    return decrypted_blocks
+    # Convert matrix back to 128-bit word
+    decrypted_block = bytes_matrix_to_word128bits(block_matrix)
+    return decrypted_block
